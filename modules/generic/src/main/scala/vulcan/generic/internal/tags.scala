ab68317f81ffbf77dfa8651c8cae8a455de62596
Ben Plommer <ben.plommer@itv.com>
2021-01-15 10:11:46 +0000

    scalafmt

--- modules/generic/src/main/scala/vulcan/generic/internal/tags.scala
+++ modules/generic/src/main/scala/vulcan/generic/internal/tags.scala
@@ -11,8 +11,7 @@ import scala.reflect.runtime.universe.WeakTypeTag
 private[generic] object tags {
   final def docFrom[A](tag: WeakTypeTag[A]): Option[String] =
     tag.tpe.typeSymbol.annotations.collectFirst {
-      case annotation if
-            annotation.tree.tpe.typeSymbol.fullName == "vulcan.generic.AvroDoc" =>
+      case annotation if annotation.tree.tpe.typeSymbol.fullName == "vulcan.generic.AvroDoc" =>
         val doc = annotation.tree.children.last.toString
         doc.substring(1, doc.length - 1)
     }
