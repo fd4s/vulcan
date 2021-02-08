805a919ca3dffd1b33e0d9e60275fc30e7101831
Ben Plommer <ben.plommer@itv.com>
2021-02-08 18:29:23 +0000

    Narrow Repr type of enum codec, use Codec.Aux more

--- modules/enumeratum/src/main/scala/enumeratum/Vulcan.scala
+++ modules/enumeratum/src/main/scala/enumeratum/Vulcan.scala
@@ -6,6 +6,8 @@
 
 package enumeratum
 
+import org.apache.avro.generic.GenericData
+
 import scala.reflect.runtime.universe.WeakTypeTag
 import vulcan.{AvroError, Codec}
 import vulcan.generic.deriveEnum
@@ -13,7 +15,7 @@ import vulcan.generic.deriveEnum
 object Vulcan {
   def enumCodec[A <: EnumEntry](
     enum: Enum[A]
-  )(implicit tag: WeakTypeTag[A]): Codec[A] = {
+  )(implicit tag: WeakTypeTag[A]): Codec.Aux[GenericData.EnumSymbol, A] = {
     lazy val typeName = tag.tpe.typeSymbol.name.decodedName
     lazy val entries = enum.values.map(_.entryName).mkString(", ")
     val notFound = (name: String) => AvroError(s"$name is not a member of $typeName ($entries)")
--- modules/enumeratum/src/main/scala/enumeratum/values/VulcanValueEnum.scala
+++ modules/enumeratum/src/main/scala/enumeratum/values/VulcanValueEnum.scala
@@ -6,6 +6,9 @@
 
 package enumeratum.values
 
+import org.apache.avro.generic.GenericData
+import org.apache.avro.util.Utf8
+
 import scala.reflect.runtime.universe.WeakTypeTag
 import vulcan.Codec
 
@@ -18,41 +21,53 @@ sealed trait VulcanValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType]] 
 trait ByteVulcanEnum[EntryType <: ByteEnumEntry] extends VulcanValueEnum[Byte, EntryType] {
   this: ValueEnum[Byte, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[Integer, EntryType] =
     Vulcan.codec(this)
 }
 
 trait CharVulcanEnum[EntryType <: CharEnumEntry] extends VulcanValueEnum[Char, EntryType] {
   this: ValueEnum[Char, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[Utf8, EntryType] =
     Vulcan.codec(this)
 }
 
 trait IntVulcanEnum[EntryType <: IntEnumEntry] extends VulcanValueEnum[Int, EntryType] {
   this: ValueEnum[Int, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[java.lang.Integer, EntryType] =
     Vulcan.codec(this)
 }
 
 trait LongVulcanEnum[EntryType <: LongEnumEntry] extends VulcanValueEnum[Long, EntryType] {
   this: ValueEnum[Long, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[java.lang.Long, EntryType] =
     Vulcan.codec(this)
 }
 
 trait ShortVulcanEnum[EntryType <: ShortEnumEntry] extends VulcanValueEnum[Short, EntryType] {
   this: ValueEnum[Short, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[java.lang.Integer, EntryType] =
     Vulcan.codec(this)
 }
 
 trait StringVulcanEnum[EntryType <: StringEnumEntry] extends VulcanValueEnum[String, EntryType] {
   this: ValueEnum[String, EntryType] =>
 
-  implicit override def vulcanCodec(implicit tag: WeakTypeTag[EntryType]): Codec[EntryType] =
+  implicit override def vulcanCodec(
+    implicit tag: WeakTypeTag[EntryType]
+  ): Codec.Aux[GenericData.EnumSymbol, EntryType] =
     Vulcan.enumCodec(this)
 }
