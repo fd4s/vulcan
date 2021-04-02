a4eb8d1aa2f7c00083c627bb9ed0f758cbbbec13
Ben Plommer <ben.plommer@itv.com>
2021-04-02 17:14:13 +0100

    scalafmt

--- modules/refined/src/test/scala/vulcan/refined/RefinedSpec.scala
+++ modules/refined/src/test/scala/vulcan/refined/RefinedSpec.scala
@@ -7,23 +7,23 @@ import org.scalacheck.Prop._
 import vulcan.Codec
 
 final class RefinedSpec extends ScalaCheckSuite with EitherValues {
-    property("Refined should succeed for values conforming to predicate") {
-      forAll { (posInt: PosInt) =>
-        val codec = Codec[PosInt]
-        val schema = codec.schema.value
-        val encoded = codec.encode(posInt).value
-        val decoded = codec.decode(encoded, schema).value
-        decoded == posInt
-      }
+  property("Refined should succeed for values conforming to predicate") {
+    forAll { (posInt: PosInt) =>
+      val codec = Codec[PosInt]
+      val schema = codec.schema.value
+      val encoded = codec.encode(posInt).value
+      val decoded = codec.decode(encoded, schema).value
+      decoded == posInt
     }
+  }
 
-    property("Refined should fail for values not conforming to predicate") {
-      forAll { (nonPosInt: NonPosInt) =>
-        val codec = Codec[PosInt]
-        val schema = codec.schema.value
-        val encoded = Codec[Int].encode(nonPosInt.value).value
-        val error = codec.decode(encoded, schema).swap.map(_.message).value
-        error == s"Predicate failed: ($nonPosInt > 0)."
-      }
+  property("Refined should fail for values not conforming to predicate") {
+    forAll { (nonPosInt: NonPosInt) =>
+      val codec = Codec[PosInt]
+      val schema = codec.schema.value
+      val encoded = Codec[Int].encode(nonPosInt.value).value
+      val error = codec.decode(encoded, schema).swap.map(_.message).value
+      error == s"Predicate failed: ($nonPosInt > 0)."
     }
+  }
 }
