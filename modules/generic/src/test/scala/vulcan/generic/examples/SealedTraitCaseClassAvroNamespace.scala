de135dbfda178191d9c3de9c082e8761e76c3c56
Ben Plommer <ben.plommer@gmail.com>
2022-03-09 13:00:13 +0000

    Revert object wrappers

--- modules/generic/src/test/scala/vulcan/generic/examples/SealedTraitCaseClassAvroNamespace.scala
+++ modules/generic/src/test/scala/vulcan/generic/examples/SealedTraitCaseClassAvroNamespace.scala
@@ -5,20 +5,14 @@ import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Arbitrary.arbitrary
 import vulcan.Codec
 import vulcan.generic._
-import A._
-import B._
 
 sealed trait SealedTraitCaseClassAvroNamespace
 
-object A {
 @AvroNamespace("com.example")
 final case class SecondInSealedTraitCaseClassAvroNamespace(value: String)
     extends SealedTraitCaseClassAvroNamespace
-}
-object B {
 final case class FirstInSealedTraitCaseClassAvroNamespace(value: Int)
     extends SealedTraitCaseClassAvroNamespace
-}
 
 object SealedTraitCaseClassAvroNamespace {
   implicit val sealedTraitCaseClassAvroNamespaceArbitrary
